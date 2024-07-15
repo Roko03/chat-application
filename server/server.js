439f164f37e9919fbdd6a9b3e6d9dcd080cf99b8
Roko <rokoponjarac2@gmail.com>
2024-07-15 19:18:37 +0200

    make error handler middleware

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlewMiddleware = require("./middleware/error-handler");
 
 app.get("/", (req, res) => {
   res.json("Ej");
 });
 
 app.use(notFoundMiddleware);
+app.use(errorHandlewMiddleware);
