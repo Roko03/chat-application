cce880e102dd1503d076fa918cab20601ee8624e
Roko <rokoponjarac2@gmail.com>
2024-07-16 17:46:26 +0200

    add style to authentication section

--- client/src/pages/authentication-page/AuthenticationPageSection.tsx
+++ client/src/pages/authentication-page/AuthenticationPageSection.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import styles from "./AuthenticationPageSection.module.scss";
 
 const AuthenticationPageSection = () => {
-  return <section>AuthenticationPageSection</section>;
+  return (
+    <section className={styles.authentication_section}>
+      <h2>Welcome to Chat application</h2>
+    </section>
+  );
 };
 
 export default AuthenticationPageSection;
