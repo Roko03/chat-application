3eefed382b19d24130420c32600b0da9670f3e75
Roko <rokoponjarac2@gmail.com>
2024-07-16 18:23:53 +0200

    refactor register schema with password validation

--- client/src/types/schema.ts
+++ client/src/types/schema.ts
@@ -7,6 +7,10 @@ export const registerSchema = z.object({
     email: z.string().min(1, { message: "Unesite email" })
         .refine((text) => patterns.email.test(text), { message: "Unesite valjani email" }),
     password: z.string().min(6, { message: 'Šifra treba imati minimalno 6 znakova' }),
+    repeatPassword: z.string().min(6, { message: 'Šifra treba imati minimalno 6 znakova' })
+}).refine((data) => data.password === data.repeatPassword, {
+    message: "Lozinke se ne poklapaju",
+    path: ["repeatPassword"]
 })
 
 export const loginSchema = z.object({
