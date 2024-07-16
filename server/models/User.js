1a602f144fc80aaf4f2ad00a518d7713e3302768
Roko <rokoponjarac2@gmail.com>
2024-07-16 11:43:04 +0200

    refactor hash password

--- server/models/User.js
+++ server/models/User.js
@@ -33,6 +33,7 @@ const UserSchema = new mongoose.Schema(
 UserSchema.pre("save", async function (next) {
   let salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const UserModel =
