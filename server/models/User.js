af44435e439619bf45f12a41eacde67530ce6f59
Roko <rokoponjarac2@gmail.com>
2024-07-16 11:37:45 +0200

    make user model

---
+++ server/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Unesite svoje korisničko ime"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        ,
+        "Unesite valjan email",
+      ],
+      required: [true, "Unesite email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Unesite šifru"],
+      minLength: [6, "Minimalan broj znakova je 6"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const UserModel =
+  mongoose.models.User || mongoose.model("User", UserSchema, "user");
+
+module.exports = UserModel;
