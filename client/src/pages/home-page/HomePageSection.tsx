08d0705f276e840bbe4a0b99e5418ab9817d0fa0
Roko <rokoponjarac2@gmail.com>
2024-07-18 17:10:42 +0200

    make header component and add logout function

--- client/src/pages/home-page/HomePageSection.tsx
+++ client/src/pages/home-page/HomePageSection.tsx
@@ -1,7 +1,13 @@
+import HeaderComponent from "../../components/header/HeaderComponent";
 import styles from "./HomePageSection.module.scss";
 
 const HomePageSection = () => {
-  return <section className={styles.home_section}>Section</section>;
+  return (
+    <>
+      <HeaderComponent />
+      <section className={styles.home_section}></section>
+    </>
+  );
 };
 
 export default HomePageSection;
