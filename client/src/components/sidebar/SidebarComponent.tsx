e8ed0589a78bd54ae25cb7564909192b0b091979
Roko <rokoponjarac2@gmail.com>
2024-07-18 19:06:37 +0200

    style sidebar

--- client/src/components/sidebar/SidebarComponent.tsx
+++ client/src/components/sidebar/SidebarComponent.tsx
@@ -1,7 +1,22 @@
+import { useAuth } from "../../util/useAuthContext";
 import styles from "./SidebarComponent.module.scss";
 
 const SidebarComponent = () => {
-  return <aside className={styles.sidebar}>SidebarComponent</aside>;
+  const auth = useAuth();
+
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.sidebar__container}>
+        <div className={styles.sidebar__list}></div>
+        {auth && (
+          <div className={styles.sidebar__profile}>
+            <p>{auth.user?.username}</p>
+            <p>{auth.user?.email}</p>
+          </div>
+        )}
+      </div>
+    </aside>
+  );
 };
 
 export default SidebarComponent;
