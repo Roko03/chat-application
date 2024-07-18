f833ae2d6173913fb8a834781f44ddce1541ecb5
Roko <rokoponjarac2@gmail.com>
2024-07-18 16:44:55 +0200

    make home page section

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes, createBrowserRouter } from "react-router-dom";
 import AuthenticationPage from "./routes/authentication/AuthenticationPage";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import HomePage from "./routes/home/HomePage";
 
 function Root() {
   return (
     <Routes>
       <Route path="/authentication" element={<AuthenticationPage />} />
       <Route element={<ProtectedRoute />}>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<HomePage />} />
       </Route>
       <Route path="*" element={<h1>Error page</h1>} />
     </Routes>
---
+++ client/src/pages/home-page/HomePageSection.tsx
@@ -0,0 +1,7 @@
+import styles from "./HomePageSection.module.scss";
+
+const HomePageSection = () => {
+  return <section className={styles.home_section}>Section</section>;
+};
+
+export default HomePageSection;
