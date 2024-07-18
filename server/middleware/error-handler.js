331edf50c0346d57c405795951cda67e93dfba83
Roko <rokoponjarac2@gmail.com>
2024-07-18 15:54:08 +0200

    make custom error handler

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -2,13 +2,40 @@ const { StatusCodes } = require("http-status-codes");
 const CustomApiError = require("../errors/custom-error");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomApiError) {
-    return res.status(err.statusCode).json({ message: err.message });
+  const customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || "Something went wrong",
+  };
+
+  if (err.name === "ValidationError") {
+    customError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
+  }
+
+  if (err.code && err.code === 11000) {
+    if (
+      err.keyPattern.user_id === 1 ||
+      err.keyPattern.organization_id === 1 ||
+      err.keyPattern.workshop_id === 1
+    ) {
+      customError.message = "Korisnik već postoji";
+    } else {
+      customError.message =
+        "Korisnik sa unesenim imenom ili emailom već postoji";
+    }
+    customError.statusCode = 400;
+  }
+
+  if (err.name === "CastError") {
+    customError.message = "Ne postoji";
+    customError.statusCode = 404;
   }
 
   return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ message: "Nešto je pošlo po krivu" });
+    .status(customError.statusCode)
+    .json({ message: customError.message });
 };
 
 module.exports = errorHandlerMiddleware;
